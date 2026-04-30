ce053ea3613cae5fb0c973cd9603e670a7246b25
marek <marekdanco.mail@gmail.com>
2026-04-30 10:57:48 +0200

    avoid diffs in fuzzer

--- testing/fuzzing/run_qfnia_fuzzer.sh
+++ testing/fuzzing/run_qfnia_fuzzer.sh
@@ -29,4 +29,4 @@ its=$1
 setup_venv
 S1='../../src/qfn2l/qf_solver.py --maxits 20 --timeout 5'
 S2='./venv/bin/z3 -T:5 -in'
-./nia_fuzzer.py -d --no-nia -j 10 ${its} "$S1" "$S2"
+./nia_fuzzer.py --no-nia -j 10 ${its} "$S1" "$S2"
